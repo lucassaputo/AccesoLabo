ebed0282fe0c1bb935aac64b2b77f09b531445ad
Pablo-Coll <112663594+Pablo-Coll@users.noreply.github.com>
2024-02-10 13:27:25 -0300

    Archivo Residente

---
+++ AccesoLabo/ArchivoResidente.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Residente.h"
+class ArchivoResidente
+{
+    public:
+        ArchivoResidente(std::string nombreArchivo);
+        bool Guardar(Residente reg);
+        bool Guardar(Residente reg, int nroRegistro);
+        int ContarRegistros();
+        Residente Leer(int nroRegistro);
+        int Buscar(int dni);
+
+    private:
+        std::string _nombreArchivo;
+   
+
+
+};
+
