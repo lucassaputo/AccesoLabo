1c634671497a4789f5d706b3f74ac9755dc85d05
lucas <lucas.saputo@gmail.com>
2024-02-17 10:49:51 -0300

    dudas

--- AccesoLabo/Source.cpp
+++ AccesoLabo/Source.cpp
@@ -12,53 +12,9 @@ ConfigSingleton* ConfigSingleton::instance = nullptr;
 
 int main() {	
 	App app;
-	app.Ejecutar();//
+	app.Ejecutar();
 
 	return 0;
 }
 
 
-/*
-* validar si esta adentro del mismo lote antes de dar salida
-* agregar estado a la validacion de las personas en registroManager, en los tres tipos
-* chequear si es inquilino una fecha que sea hasta hay que agregar y dsp validar
-* usar <vector> en vez de dinamica
-* ABML vehiculos
-* ver cuando pasar por referencia objetos o solo pasar el atributo que utilice la funcion
-* validar el ingrese de opciones del menu que sean solo numeros
-* validar password usuario
-* maximo de caracteres en legajo
-* estado activo o no es de usuario o persona? o de ambos?
-* como se hace referencia a un atributo de la clase padre en el cpp de la hija? quiero acceder a _dni pero no pude, sera pq el encapsulamiento? this no funciono
-* metodo para ingresar pass y limitar cant de caracteres
-* permirtir numeros en observaciones unidad
-* tocas y chequear tamaño de los atributos char[xxxxx]
-* limitar mas los submenus segun usuario logueado
-* poner en rojo algunos textos? o negritas?
-* chequear defaults en switch que tengan todos
-* discriminar errores en buscarObj por ejemplo da igual sino abrio el archivo o si no encontro  registro
-* tipo proveedor, lo sacamos?
-* como reconoce proveedor si no esta incluido en abmPersonasManager
-* pasar a texto en edicion la condicion se es inquilino o propietario
-* corregir o ver que autorizacion queda vigente si la persona se da de baja o no?
-*/
-
-/*
-* tipos de unidades:
-* --------------------
-* sentido 1 entrada 2 salida
-* 
-* nivel usuarios:
-* --------------------
-* 1 admin
-* 2 vigilador
-* 
-* tipos de personas;:
-* --------------------
-* 1: Visita | 2: Proveedor | 3: Residente
-*
-* usuarios
-* --------------
-* user leg 8 pass 12 ADMIN
-* user 10 pass 1234 VIGILADOR
-*/
